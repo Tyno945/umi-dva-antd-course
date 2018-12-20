ab3af7a3c67f208f946d9523c724033b59924056
Felix <441356417@qq.com>
2018-12-20 14:43:51 +0800

    基本布局

--- config/config.js
+++ config/config.js
@@ -2,11 +2,17 @@ export default {
   singular: true,
   plugins: [
     ['umi-plugin-react', {
-
+      antd: true
     }],
   ],
   routes: [{
     path: '/',
-    component: './HelloWorld',
+    component: '../layout',
+    routes: [
+      {
+        path: '/',
+        component: './HelloWorld'
+      },
+    ]
   }],
 };
